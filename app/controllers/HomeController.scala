eee4496d0f833df4dfe1d7a16e19eb1821fb46a0
Piotr Bugara <piotr.bugara@gmail.com>
2019-01-05 14:58:57 +0100

    Created default index page and header.

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import javax.inject._
+
+import play.api.mvc._
+
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents) (implicit assetsFinder: AssetsFinder)
+  extends AbstractController(cc) {
+
+  def index = Action {
+    Ok(views.html.index("Strona główna projektu DSL"))
+  }
+
+}
